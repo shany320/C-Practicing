7588dc1a03de0533ee1befade79078640369a546
shany320 <154510107+shany320@users.noreply.github.com>
2026-03-05 15:41:21 +0200

    Uploading c# basics and types

---
+++ Assignment(Basics#1)/Assignment01/Program.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+
+namespace Assignment01
+{
+
+    class Program
+    {
+       static void Main()
+        {
+            // Program 1
+            //Console.WriteLine("Enter a number: ");
+            //int input = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"You have entered a number {input}");
+
+            //Program 2
+            //Console.WriteLine("Enter a non numeric character: ");
+            //int input;
+            //int.TryParse(Console.ReadLine(),out input);
+            //Console.WriteLine($"You have entered a number {input}");
+
+            //Program 3
+            //double x = 3.5, y = 2.5, z;
+            //z = x + y;
+            //Console.WriteLine(z);
+
+            //Program 4
+            //String x = "Saif Hany";
+            //Console.WriteLine(x.Substring(4));
+            //Console.WriteLine(x.Substring(0,4));
+
+            //Program 5
+            //int x, y, z;
+            //x = 5;
+            //y = x;
+            //z = x + y;
+            //Console.WriteLine($"x={x},y={y},z={z}");
+            //z = x;
+            //y = z - x;
+            //Console.WriteLine($"x={x},y={y},z={z}");
+
+            //Program 6
+            //int[] arr1 = { 1, 2, 3, 4, 5 };
+            //int[] arr2 = arr1;
+            //Console.WriteLine($"arr1:{arr1[1]} and arr2:{arr2[1]}");
+            //arr2[1] = 6;
+            //Console.WriteLine($"arr1:{arr1[1]} and arr2:{arr2[1]}");
+
+            //Program 7
+            //Console.WriteLine("Write Your First Name");
+            //String word1 = Console.ReadLine();
+            //Console.WriteLine("Write Your Last Name");
+            //String word2 = Console.ReadLine();
+            //Console.WriteLine(word1+" "+word2);
+
+            //Question 8:
+            //Answer is (B) as the line output would be true which equals to one
+
+            //Question 9:
+            //The answer is (D) as the division of 13 by 6 will give 6 while the value given out of modulus is one which means that the vaue returns with remainder of 1
+
+            //Question 10:
+            // The answer is (D) as The true condition in the if statement will be implemented and ++num=2, z++=5 and ++z=7
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+    }
